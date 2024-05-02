15957aa9ccda473730e4d625ca64f93c60007131
Calvin Wilkinson <kinsondigital@gmail.com>
2024-05-02 16:58:13 +0100

    config, refactor: setup indentation rules and apply them

--- app/greet.tsx
+++ app/greet.tsx
@@ -4,13 +4,13 @@ import { useEffect, useState } from "react";
 import { invoke } from "@tauri-apps/api/tauri";
 
 export default function Greet() {
-    const [greeting, setGreeting] = useState("");
+	const [greeting, setGreeting] = useState("");
 
-    useEffect(() => {
-        invoke<string>("greet", { name: "Kinson" })
-            .then((result) => setGreeting(result))
-            .catch(console.error);
-    }, []);
+	useEffect(() => {
+		invoke<string>("greet", { name: "Kinson" })
+			.then((result) => setGreeting(result))
+			.catch(console.error);
+	}, []);
 
-    return <div>{greeting}</div>
+	return <div>{greeting}</div>
 }
