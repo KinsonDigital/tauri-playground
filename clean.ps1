c956da3e1f9a6f46cd4fea78839c42bb3c2c8d9b
Calvin Wilkinson <kinsondigital@gmail.com>
2024-05-02 15:10:45 +0100

    wip

--- clean.ps1
+++ clean.ps1
@@ -1,19 +1,36 @@
 # Used to clean up all of the build output and dependency directories by deleting them
 
+Write-Host "Cleaning output & dependency directories..." -ForegroundColor Cyan;
+$cwd = Get-Location;
 
-# Remove the '.next' folder
-Write-Host "Removing the '.next' folder..." -ForegroundColor DarkGray;
-# Get-ChildItem -Path "./.next" | Remove-Item -Recurse -Force;
-Remove-Item -Path "./.next" -Force -Recurse;
+# # Remove the '.next' folder
+# Write-Host "   Removing the '.next' folder..." -ForegroundColor DarkGray;
+
+# $nextDirPath = "$cwd/.next";
+# if (Test-Path -Path $nextDirPath) {
+#     Remove-Item -Path $nextDirPath -Force -Recurse - -Confirm:$false;
+# } else {
+#     Write-Host "   The '.next' folder does not exist." -ForegroundColor DarkGray;
+# }
 
 # Remove the 'node_modules' folder
-Write-Host "Removing the 'node_modules' folder..." -ForegroundColor DarkGray;
-# Get-ChildItem -Path "./node_modules" | Remove-Item -Recurse -Force;
-Remove-Item -Path "./node_modules" -Force -Recurse;
+Write-Host "   Removing the 'node_modules' folder..." -ForegroundColor DarkGray;
 
-# Remove the 'out' folder
-Write-Host "Removing the 'out' folder..." -ForegroundColor DarkGray;
-# Get-ChildItem -Path "./out" | Remove-Item -Recurse -Force;
-Remove-Item -Path "./out" -Force -Recurse;
+$nodeModulesDirPath = "$cwd/node_modules";
+if (Test-Path -Path $nodeModulesDirPath) {
+    Remove-Item -Path $nodeModulesDirPath -Force -Recurse -Confirm:$false;
+} else {
+    Write-Host "   The 'node_modules' folder does not exist." -ForegroundColor DarkGray;
+}
+
+# # Remove the 'out' folder
+# Write-Host "   Removing the 'out' folder..." -ForegroundColor DarkGray;
+
+# $outDirPath = "$cwd/out";
+# if (Test-Path -Path $outDirPath) {
+#     Remove-Item -Path $outDirPath -Force -Recurse -Confirm:$false;
+# } else {
+#     Write-Host "   The 'out' folder does not exist." -ForegroundColor DarkGray;
+# }
 
 Write-Host "Everything Clean!" -ForegroundColor DarkGreen;
