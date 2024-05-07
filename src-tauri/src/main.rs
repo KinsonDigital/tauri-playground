24222891ac6459241f146d4dac877fd3d4e4702a
Calvin Wilkinson <kinsondigital@gmail.com>
2024-05-07 14:05:02 +0100

    asdf

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,11 +1,18 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+// #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+use tauri::RunEvent;
 
 fn main() {
-  tauri::Builder::default()
-    .invoke_handler(tauri::generate_handler![greet])
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![greet])
+        .build(tauri::generate_context!()) // build instead of run
+        .expect("error while building tauri app")
+        .run(|_app_handle, event| {
+            if let RunEvent::Updater(updater_event) = event {
+                dbg!(updater_event);
+            }
+        });
 
     println!("Message from rust!!");
 }
@@ -14,4 +21,3 @@ fn main() {
 fn greet(name: &str) -> String {
     format!("Hello, {}!", name)
 }
-
